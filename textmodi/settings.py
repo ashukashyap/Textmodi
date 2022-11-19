7d0782739bcb51a4e592f995ec24a0c9d5e10747
ashukashyap <63107983+ashukashyap@users.noreply.github.com>
2022-11-18 23:23:32 -0800

    Update settings.py

--- textmodi/settings.py
+++ textmodi/settings.py
@@ -79,6 +79,16 @@ WSGI_APPLICATION = 'textmodi.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'textmodidb',
+        'USER': 'adminuser',
+        'PASSWORD': 'testing@123',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 
 
